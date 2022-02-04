5dfe5dcfa735e31741558e323b7babc6c5501222
MelissaCFox <melissa.caitlin.fox@gmail.com>
2022-02-04 15:40:37 -0600

    users can upload game images from game details

--- src/game/GameDetail.js
+++ src/game/GameDetail.js
@@ -13,32 +13,59 @@ export const GameDetail = (props) => {
         rating: 5
     })
     const [newRating, setNewRating] = useState(false)
+    const [gameImage, setGameImage] = useState({
+        gameId: parseInt(gameId),
+        gameImage: ""
+    })
 
     useEffect(() => {
-        getGame(gameId).then(data => setGame(data))
+        getGame(gameId).then(data => {
+            setGame(data)
+            setImgSrc(data.image[0].image)
+        })
+        
     }, [newRating])
 
     const setRating = (event) => {
-        const copy = {...gameRating}
+        const copy = { ...gameRating }
         copy.rating = parseInt(event.target.value)
         setGameRating(copy)
     }
 
     const submitRating = () => {
         createRating(gameRating)
-        .then(() => {
-            setNewRating(!newRating)
-            document.getElementById("gameRating").value = "5"
-            const copy = {...gameRating}
-            copy.rating = 5
-            setGameRating(copy)
-        })
+            .then(() => {
+                setNewRating(!newRating)
+                document.getElementById("gameRating").value = "5"
+                const copy = { ...gameRating }
+                copy.rating = 5
+                setGameRating(copy)
+            })
     }
+
+    const getBase64 = (file, callback) => {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => callback(reader.result));
+        reader.readAsDataURL(file);
+    }
+    
+    const createGameImageString = (event) => {
+        getBase64(event.target.files[0], (base64ImageString) => {
+            console.log("Base64 of file is", base64ImageString);
+            const copy = {...gameImage}
+            copy.gameImage = base64ImageString
+            setGameImage(copy)
+        });
+    }
+    
+    const [imgSrc, setImgSrc] = useState("")
+    
 
     return (
         <>
             <section key={`game--${game.id}`} className="game">
                 <div className="game__title">{game.title} by {game.designer}</div>
+                <img src={imgSrc}></img>
                 <div className="game__year">Released in {game.year_released}</div>
                 <div className="game__description">{game.description}</div>
                 <div className="game__averageRating">Average Player Rating: {game.average_rating}{game.average_rating === "No Ratings Yet" ? "" : "/10"}</div>
@@ -52,18 +79,26 @@ export const GameDetail = (props) => {
                         })
                     }
                 </div>
-                
+
                 <div className="userRating">Rate this Game:
-                <input type="range" min="1" max="10" defaultValue="5" className="slider" id="gameRating" onChange={setRating}/>
-                ({gameRating.rating}/10)
-                <button onClick={submitRating}>Submit Rating</button>
+                    <input type="range" min="1" max="10" defaultValue="5" className="slider" id="gameRating" onChange={setRating} />
+                    ({gameRating.rating}/10)
+                    <button onClick={submitRating}>Submit Rating</button>
+                </div>
+
+                <div>Upload an Image
+                <input type="file" id="game_image" onChange={createGameImageString} />
+                <input type="hidden" name="game_id" value={game.id} />
+                <button onClick={() => {
+                    // Upload the stringified image that is stored in state
+                }}>Upload</button>
                 </div>
 
                 <div className="game__reviews">Reviews:
                     {
                         game.reviews?.map((review) => {
                             return <div key={review.id}><div className="game__review">{review.review}</div>
-                            <button onClick={() => history.push(`/games/${game.id}/review/${review.id}`)}>Edit</button>
+                                <button onClick={() => history.push(`/games/${game.id}/review/${review.id}`)}>Edit</button>
                             </div>
                         })
                     }
